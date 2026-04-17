845b10241ff9671b7fa5e2a32f2f1030511dee3d
Tzielinskii <zielinski.tomasz@zst.radom.pl>
2026-04-17 09:01:17 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+$a = 25;
+
+echo $a . "<br>";
+
+//zad 2
+echo -$a . "<br>";
+
+//zad 3
+echo $a + 3 . "<br>";
+
+//zad 4
+echo $a - 5 . "<br>";
+
+//zad 5
+echo $a * 2 . "<br>";
+
+//zad 6
+echo $a / 5 . "<br>";
+
+//zad 7
+echo $a % 2 . "<br>";
+
+//zad 8
+echo pow($a, 3) . "<br>";
+
+echo "wartość zmiennej się nie zmieni" . $a . " i już się nie zmieni";
+?>
